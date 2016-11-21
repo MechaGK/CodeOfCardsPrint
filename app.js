be384e3dbd2474cb7510a94e31cb382a8f534d21
Mads Grau Kristensen <madsgrau@gmail.com>
2016-11-21 14:50:32 +0100

    Moved script to app.js

---
+++ app.js
@@ -0,0 +1,48 @@
+function frequencySum() {
+	var sum = 0;
+
+	for (var i = cards.length - 1; i >= 0; i--) {
+		sum += cards[i].rarity;
+	}
+
+	return sum;
+}
+
+function calcCardsCount() {
+	frequencySum = frequencySum();
+
+	for (var i = cards.length - 1; i >= 0; i--) {
+		cards[i].count = Math.floor( (cards[i].rarity / frequencySum) * decksize )
+	}
+}
+
+var decksize = 150;
+
+cards = [ { text: "DrawCard(Self)", rarity: 10}, { text: "DrawCard(Other)", rarity: 10}, {text: "Function.Cycles--", rarity: 5}];
+
+calcCardsCount();
+
+var counter = 0;
+var pageElement = document.createElement("div");
+pageElement.className = "page";
+
+for (var i = cards.length - 1; i >= 0; i--) {
+	for (var j = cards[i].count - 1; j >= 0; j--) {
+		if (counter >= 30) {
+			document.body.appendChild(pageElement);
+			pageElement = document.createElement("div");
+			pageElement.className = "page";
+
+			counter = 0;
+		}
+
+		var div = document.createElement("div");
+		div.className = "card";
+		div.innerHTML = cards[i].text + " " + (j+1);
+
+		pageElement.appendChild(div);
+		counter++;
+	}
+}
+
+document.body.appendChild(pageElement);
